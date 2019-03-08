7598357d255d4e33e17517152e1cc4382a3c6351
Carsten Fortmann-Grote <carsten.fortmann-grote@evolbio.mpg.de>
2019-03-08 15:17:35 +0100

    Adding setup.py.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='GenDBScraper',
+    version='0.1dev',
+    packages=['GenDBScraper',],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
